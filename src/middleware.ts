9cc0f48e17042f0f37086099b0e678b43b1e52d1
mejzerVitaliy <mejzervitalik@gmail.com>
2024-11-02 10:44:52 +0200

    fix redirecting to home page

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
     console.log('middleware is working');
      
-    // const userID = req.cookies.get('UserID')?.value
-    // console.log("UserID from cookies:", userID);
-    // console.log("Requested path:", req.nextUrl.pathname);
+    const userID = req.cookies.get('UserID')?.value
+    console.log("UserID from cookies:", userID);
+    console.log("Requested path:", req.nextUrl.pathname);
 
-    // if (userID && req.nextUrl.pathname === '/signIn') {
-    //     console.log("Redirecting to home page...");
-    //     return NextResponse.redirect(new URL('/', req.url))
-    // }
-    // return NextResponse.next()
+    if (userID && (req.nextUrl.pathname === '/signIn' || '/logIn')) {
+        console.log("Redirecting to home page...");
+        return NextResponse.redirect(new URL('/', req.url))
+    }
+    return NextResponse.next()
 }
 
 export const config = {
